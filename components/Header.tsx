818e9156dff2c4716c107e46387a0952ba4cb545
dainy1007 <dainy1007@gmail.com>
2026-09-01 14:22:15 +0900

    확정 로고 버전 갱신

--- components/Header.tsx
+++ components/Header.tsx
@@ -74,7 +74,7 @@ export default function Header() {
             aria-label="백조현대부동산중개 홈"
           >
             <img
-              src="/brand/baekjo-approved-logo.webp?v=20260901-1422"
+              src="/brand/baekjo-approved-logo.webp?v=20260901-1425"
               alt="백조현대부동산중개"
               className="h-[72px] w-auto object-contain sm:h-[86px] xl:h-[102px] 2xl:h-[106px]"
             />
