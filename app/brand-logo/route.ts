a5e0801111e7dec099d532e27abd5c6c4fdcc390
dainy1007 <dainy1007@gmail.com>
2026-09-01 14:59:38 +0900

    로고 데이터 조각 최종 무결성 수정

--- app/brand-logo/route.ts
+++ app/brand-logo/route.ts
@@ -1,5 +1,6 @@
 import c0a from "../../lib/brand-logo/chunk0a";
-import c0b from "../../lib/brand-logo/chunk0b";
+import c0b1 from "../../lib/brand-logo/chunk0b1";
+import c0b2 from "../../lib/brand-logo/chunk0b2";
 import c1 from "../../lib/brand-logo/chunk1";
 import c2 from "../../lib/brand-logo/chunk2";
 import c3 from "../../lib/brand-logo/chunk3";
@@ -12,7 +13,7 @@ import c8 from "../../lib/brand-logo/chunk8";
 export const runtime = "nodejs";
 
 export async function GET() {
-  const base64 = [c0a, c0b, c1, c2, c3, c4, c5, c6, c7, c8].join("");
+  const base64 = [c0a, c0b1, c0b2, c1, c2, c3, c4, c5, c6, c7, c8].join("");
   const bytes = Buffer.from(base64, "base64");
 
   return new Response(bytes, {
