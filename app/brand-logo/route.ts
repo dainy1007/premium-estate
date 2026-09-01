6131c23376a2f4cdfa49107eaabed9d18e43467f
dainy1007 <dainy1007@gmail.com>
2026-09-01 14:52:28 +0900

    확정 로고를 안전하게 제공하는 이미지 라우트 추가

---
+++ app/brand-logo/route.ts
@@ -0,0 +1,24 @@
+import c0 from "../../lib/brand-logo/chunk0";
+import c1 from "../../lib/brand-logo/chunk1";
+import c2 from "../../lib/brand-logo/chunk2";
+import c3 from "../../lib/brand-logo/chunk3";
+import c4 from "../../lib/brand-logo/chunk4";
+import c5 from "../../lib/brand-logo/chunk5";
+import c6 from "../../lib/brand-logo/chunk6";
+import c7 from "../../lib/brand-logo/chunk7";
+import c8 from "../../lib/brand-logo/chunk8";
+
+export const runtime = "nodejs";
+
+export async function GET() {
+  const base64 = [c0, c1, c2, c3, c4, c5, c6, c7, c8].join("");
+  const bytes = Buffer.from(base64, "base64");
+
+  return new Response(bytes, {
+    headers: {
+      "Content-Type": "image/webp",
+      "Content-Length": String(bytes.length),
+      "Cache-Control": "public, max-age=31536000, immutable",
+    },
+  });
+}
