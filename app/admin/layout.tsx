3a6a9780226fe03dddef349100f7e93116a3e771
dainy1007 <dainy1007@gmail.com>
2026-09-17 17:03:38 +0900

    Show mobile push opt-in in admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import AdminShell from "@/components/AdminShell";
+import AdminPushButton from "@/components/AdminPushButton";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
-  return <AdminShell>{children}</AdminShell>;
+  return <AdminShell><div className="fixed bottom-4 right-4 z-[70]"><AdminPushButton /></div>{children}</AdminShell>;
 }
